04497d8d43309edbbba4828553eeda311cc9e200
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-03-09 23:54:14 +0900

    Title: Date to Date, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250309/Date to Date/date-to-date.py
+++ 250309/Date to Date/date-to-date.py
@@ -4,10 +4,10 @@ num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 answer = 0
 
 for month in range(m1, m2 + 1):
-    if (month == m1):
-        answer = answer + num_of_days[month] - d1 + 1
-    elif (month == m2):
+    if (month == m2):
         answer = answer + d2
+    elif (month == m1):
+        answer = answer + num_of_days[month] - d1 + 1
     else:
         answer = answer + num_of_days[month]
 
