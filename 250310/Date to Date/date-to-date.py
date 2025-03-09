cc7a99606dacc0acdc46ecb4144fcf6708f6967e
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-03-10 00:03:11 +0900

    Title: Date to Date, Time: 40ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250310/Date to Date/date-to-date.py
@@ -0,0 +1,18 @@
+m1, d1, m2, d2 = map(int, input().split())
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+answer = 0
+
+for month in range(m1, m2 + 1):
+    if m1 == m2:
+        answer = d2 - d1 + 1
+        break
+
+    if (month == m2):
+        answer = answer + d2
+    elif (month == m1):
+        answer = answer + num_of_days[month] - d1 + 1
+    else:
+        answer = answer + num_of_days[month]
+
+print(answer)
