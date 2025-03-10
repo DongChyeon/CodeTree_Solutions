b0b49786ea7f213e7db9c45176b94f6b9383d772
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-03-11 00:08:27 +0900

    Title: 진수 to 진수, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250311/진수 to 진수/transformation-of-number-system.py
@@ -0,0 +1,25 @@
+a, b = map(int, input().split())
+n = input()
+
+def from_base_to_decimal(n, b):
+    num = 0
+
+    for i in range(len(n)):
+        num += (b ** i * int(n[-1 - i]))
+
+    return num
+
+def from_decimal_to_base(n, b):
+    number = n
+    digits = []
+
+    while True:
+        if number < b:
+            digits.append(str(number))
+            break
+        digits.append(str(number % b))
+        number //= b
+    
+    return ''.join(digits)
+
+print(from_decimal_to_base(from_base_to_decimal(n, a), b))
