0b77e3bd59eddbe1d7f6eb6cdaa896d678683541
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-03-11 13:11:17 +0900

    Title: 왔다 갔던 구역 2, Time: 48ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250311/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,29 @@
+n = int(input())
+x = []
+dir = []
+for _ in range(n):
+    xi, di = input().split()
+    x.append(int(xi))
+    dir.append(di)
+
+field = [0] * 2001
+current_index = 1000
+
+for i in range(n):
+    direction = dir[i]
+    dx = x[i]
+
+    for _ in range(dx):
+        if direction == 'L':
+            current_index -= 1
+            field[current_index] += 1
+        else:
+            current_index += 1
+            field[current_index] += 1
+
+answer = 0
+for area in field:
+    if (area > 1):
+        answer += 1
+
+print(answer)
