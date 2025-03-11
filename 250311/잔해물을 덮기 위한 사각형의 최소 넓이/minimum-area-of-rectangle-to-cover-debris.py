091ebcc011e1160dc1b8f4264881efcde65da166
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-03-11 23:06:03 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 97ms, Memory: 48MB, Status: Passed - CodeTree

--- 250311/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 250311/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -14,13 +14,19 @@ for y in range(y1[1] + offset, y2[1] + offset):
     for x in range(x1[1] + offset, x2[1] + offset):
         field[y][x] = 2
 
-max_x = 0
-max_y = 0
+min_x, max_x, min_y, max_y = 2001, 0, 2001, 0
+is_first_rect_exist = False
 
 for y in range(y1[0] + offset, y2[0] + offset):
     for x in range(x1[0] + offset, x2[0] + offset):
         if field[y][x] == 1:
+            is_first_rect_exist = True
+            min_x = min(min_x, x)
             max_x = max(max_x, x)
+            min_y = min(min_y, y)
             max_y = max(max_y, y)
 
-print((max_x - offset + 1 - x1[0]) * (max_y - offset + 1 - y1[0]))
+if is_first_rect_exist:
+    print((max_x - min_x + 1) * (max_y - min_y + 1))
+else:
+    print(0)
