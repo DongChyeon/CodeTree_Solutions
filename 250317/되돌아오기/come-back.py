8626d08061a169d2cb301da362582f9fa121b4bb
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-03-17 21:26:21 +0900

    Title: 되돌아오기, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250317/되돌아오기/come-back.py
+++ 250317/되돌아오기/come-back.py
@@ -1,30 +1,31 @@
-N = int(input())
-x, y = 0, 0
-
-dx = [-1, 0, 0, 1]
-dy = [0, 1, -1, 0]
-
-mapper = {
-    'W': 0,
-    'S': 1,
-    'N': 2,
-    'E': 3,
-}
-
 def solution(n):
+    x, y = 0, 0
+
+    dx = [-1, 0, 0, 1]
+    dy = [0, 1, -1, 0]
+
+    mapper = {
+        'W': 0,
+        'S': 1,
+        'N': 2,
+        'E': 3,
+    }
+
     time = 0
 
-    direction, distance = input().split()
+    for i in range(n):
+        direction, distance = input().split()
 
-    for _ in range(int(distance)):
-        time += 1
-        nx, ny = x + dx[mapper[direction]], y + dy[mapper[direction]]
+        for _ in range(int(distance)):
+            time += 1
+            nx, ny = x + dx[mapper[direction]], y + dy[mapper[direction]]
 
-        x, y = nx, ny
-        
-        if x == 0 and y == 0:
-            return time
+            x, y = nx, ny
+            
+            if x == 0 and y == 0:
+                return time
 
     return -1    
 
+N = int(input())
 print(solution(N))
