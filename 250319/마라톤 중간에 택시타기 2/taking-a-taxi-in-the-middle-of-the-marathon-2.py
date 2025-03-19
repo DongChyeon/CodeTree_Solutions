94d06ed6e0d110a98284f143f2c0ad4fb3424eae
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-03-19 12:33:10 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250319/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 250319/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -4,8 +4,9 @@ x = [p[0] for p in points]
 y = [p[1] for p in points]
 
 answer = int(10e9)
+
 for skip_check in range(1, n - 1):
-    user_x, user_y = 0, 0
+    user_x, user_y = x[0], y[0]
     current = 0
     for i in range(1, n):
         if i == skip_check:
