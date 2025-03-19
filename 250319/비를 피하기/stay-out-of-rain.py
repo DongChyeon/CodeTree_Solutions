6cc6e36a0712b89d72c193988ce37735e16e076a
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-03-19 14:25:57 +0900

    Title: 비를 피하기, Time: 115ms, Memory: 21MB, Status: Passed - CodeTree

--- 250319/비를 피하기/stay-out-of-rain.py
+++ 250319/비를 피하기/stay-out-of-rain.py
@@ -2,36 +2,45 @@ from collections import deque
 
 n, h, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
+start_pos = [
+    (j, i)
+    for i in range(n)
+    for j in range(n)
+    if grid[i][j] == 3
+]
+step = [[0] * n for _ in range(n)]
 
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
 
-def bfs(x, y):
-    dx = [0, 0, -1, 1]
-    dy = [-1, 1, 0, 0]
+queue = deque([])
+visited = [[False] * n for _ in range(n)]
 
-    queue = deque([(0, x, y)])
-    visited = [[False] * n for _ in range(n)]
+for pos in start_pos:
+    x, y = pos
+    queue.append((0, x, y))
     visited[y][x] = True
 
-    while queue:
-        second, x, y = queue.popleft()
-        if grid[y][x] == 3:
-            return second
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
+while queue:
+    second, x, y = queue.popleft()
+    
+    if grid[y][x] == 2:
+        step[y][x] = second
 
-            if in_range(nx, ny) and not visited[ny][nx] and grid[y][x] != 1:
-                visited[ny][nx] = True
-                queue.append((second + 1, nx, ny))
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
 
-    return -1
+        if in_range(nx, ny) and not visited[ny][nx] and grid[ny][nx] != 1:
+            visited[ny][nx] = True
+            queue.append((second + 1, nx, ny))
 
 for y in range(n):
     for x in range(n):
-        if grid[y][x] == 2:
-            print(bfs(x, y), end=' ')
+        if grid[y][x] == 2 and not visited[y][x]:
+            print(-1, end=' ')
         else:
-            print(0, end=' ')
+            print(step[y][x], end=' ')
     print()
