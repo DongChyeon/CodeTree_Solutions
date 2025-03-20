c12359815b4a1ba46d7f61c2c96a20bbe2ebff16
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-03-20 17:47:35 +0900

    Title: 2명의 도둑, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250320/2명의 도둑/two-thieves.py
+++ 250320/2명의 도둑/two-thieves.py
@@ -1,85 +1,59 @@
 n, m, c = map(int, input().split())
-things = [list(map(int, input().split())) for _ in range(n)]
+weight = [list(map(int, input().split())) for _ in range(n)]
 
+best_val = [
+    [-1] * (n - m + 1)
+    for _ in range(n)
+]
+
+temp = list()
+max_val = 0
 answer = 0
 
-def get_combinations_with_repetition(n, count):
-    thing_combinations = []
-    thing_comb = []
+def find_max_val(curr_depth, curr_weight, curr_val):
+    global max_val
 
-    def choose_thing(depth, start, n):
-        if depth == count:
-            thing_combinations.append(thing_comb[:])
-            return
-        for i in range(n):
-            thing_comb.append(i)
-            choose_thing(depth + 1, i, n)
-            thing_comb.pop()
-    choose_thing(0, 0, n)
+    if curr_depth == m:
+        if curr_weight <= c:
+            max_val = max(max_val, curr_val)
+        return
 
-    return thing_combinations
+    find_max_val(curr_depth + 1, curr_weight, curr_val)
 
-def get_combinations(n, start_area, count):
-    thing_combinations = []
-    thing_comb = []
+    find_max_val(curr_depth + 1, curr_weight + temp[curr_depth], curr_val + temp[curr_depth] ** 2)
 
-    start = start_area
-    def choose_thing(depth, start, n):
-        if depth == count:
-            thing_combinations.append((start_area, thing_comb[:]))
-            return
-        for i in range(start, n):
-            thing_comb.append(i)
-            choose_thing(depth + 1, i + 1, n)
-            thing_comb.pop()
-    choose_thing(0, start, n)
+def find_max(x, y):
+    global temp, max_val
 
-    return thing_combinations
+    if best_val[y][x] != -1:
+        return best_val[y][x]
 
-def in_range(x, y):
-    return 0 <= x < n and 0 <= y < n
+    temp = weight[y][x:x + m]
 
-def calc(thiefs_x_combinations, thiefs_y):
     max_val = 0
+    find_max_val(0, 0, 0)
 
-    for comb_1 in thiefs_x_combinations:
-        for comb_2 in thiefs_x_combinations:
-            visited = [[False] * n for _ in range(n)]
-
-            if thiefs_y[0] == thiefs_y[1] and abs(comb_1[0] - comb_2[0]) < m:
-                continue
-
-            weight = 0
-            for x_1 in comb_1[1]:
-                if in_range(x_1, thiefs_y[0]) and weight + things[thiefs_y[0]][x_1] <= c and not visited[thiefs_y[0]][x_1]:
-                    visited[thiefs_y[0]][x_1] = True
-                    weight += things[thiefs_y[0]][x_1]
-
-            weight = 0
-            for x_2 in comb_2[1]:
-                if in_range(x_2, thiefs_y[1]) and weight + things[thiefs_y[1]][x_2] <= c and not visited[thiefs_y[1]][x_2]:
-                    visited[thiefs_y[1]][x_2] = True
-                    weight += things[thiefs_y[1]][x_2]
-
-            value = 0
-            for y in range(n):
-                for x in range(n):
-                    if visited[y][x]:
-                        value += things[y][x] ** 2
-
-            max_val = max(max_val, value)
-            
+    best_val[y][x] = max_val
+    
     return max_val
 
-combinations = get_combinations_with_repetition(n, 2)
-thiefs_x_combinations = []
-for i in range(1, m + 1):
-    for j in range(n - m + 1):
-        for comb in get_combinations(j + m, j, i):
-            if comb[1][-1] - comb[1][0] < m:
-                thiefs_x_combinations.append(comb)
-                     
-for thiefs_y in combinations:
-    answer = max(answer, calc(thiefs_x_combinations, thiefs_y))
+def intersect(start1, end1, start2, end2):
+    return not (end1 < start2 or end2 < start1)
+
+def is_posssible(x1, y1, x2, y2):
+    if y1 != y2:
+        return True
+
+    if intersect(x1, x1 + m - 1, x2, x2 + m - 1):
+        return False
+
+    return False
+
+for y1 in range(n):
+    for x1 in range(n - m + 1):
+        for y2 in range(2):
+            for x2 in range(n - m + 1):
+                if is_posssible(x1, y1, x2, y2):
+                    answer = max(answer, find_max(x1, y1) + find_max(x2, y2))
 
 print(answer)
