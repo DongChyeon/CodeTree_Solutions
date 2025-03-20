646d580630e74a2741577dbd9cbb2c95f0a64ad2
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-03-20 19:28:47 +0900

    Title: 알파벳과 사칙연산, Time: 5066ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

--- 250320/알파벳과 사칙연산/calculations-with-alphabet.py
+++ 250320/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -9,40 +9,37 @@ operands = [
 
 answer = -sys.maxsize - 1
 
-number_combinations = []
-numbers = []
-def choose_number(depth, n):
+def is_operator(ch):
+    return ch == '+' or ch == '-' or ch == '*'
+
+def choose_number(depth, numbers, n):
+    global answer
+
     if depth == len(operands):
-        number_combinations.append(numbers[:])
+        operand_dict = dict(zip(set(operands), numbers[:]))
+
+        val = operand_dict[equation[0]]
+        for i in range(1, len(equation)):
+            if is_operator(equation[i]):
+                operator = equation[i]
+                if operator == '+':
+                    val += operand_dict[equation[i + 1]]
+                    i += 1
+                elif operator == '-':
+                    val -= operand_dict[equation[i + 1]]
+                    i += 1
+                elif operator == '*':
+                    val *= operand_dict[equation[i + 1]]
+                    i += 1
+        
+        answer = max(answer, val)
         return
 
     for i in range(1, n + 1):
         numbers.append(i)
-        choose_number(depth + 1, n)
+        choose_number(depth + 1, numbers, n)
         numbers.pop()
-choose_number(0, 4)
 
-def is_operator(ch):
-    return equation[i] == '+' or equation[i] == '-' or equation[i] == '*'
-
-for combination in number_combinations:
-    operand_dict = dict(zip(set(operands), combination))
-
-    val = operand_dict[equation[0]]
-
-    for i in range(1, len(equation)):
-        if is_operator(equation[i]):
-            operator = equation[i]
-            if operator == '+':
-                val += operand_dict[equation[i + 1]]
-                i += 1
-            elif operator == '-':
-                val -= operand_dict[equation[i + 1]]
-                i += 1
-            elif operator == '*':
-                val *= operand_dict[equation[i + 1]]
-                i += 1
-    
-    answer = max(answer, val)
+choose_number(0, [], 4)
 
 print(answer)
