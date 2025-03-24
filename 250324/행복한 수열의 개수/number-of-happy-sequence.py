700306134b0f6dde61b3b5936163c63d1b63cdba
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-03-24 21:19:07 +0900

    Title: 행복한 수열의 개수, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250324/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,34 @@
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+answer = 0
+
+for y in range(n):
+    curr = grid[y][0]
+    curr_cnt = 1
+    if curr_cnt >= m:
+        answer += 1
+        continue
+
+    for x in range(1, n):
+        if grid[y][x] == curr:
+            curr_cnt += 1
+            if curr_cnt >= m:
+                answer += 1
+                break
+
+for x in range(n):
+    curr = grid[0][x]
+    curr_cnt = 1
+    if curr_cnt >= m:
+        answer += 1
+        continue
+        
+    for y in range(1, n):
+        if grid[y][x] == curr:
+            curr_cnt += 1
+            if curr_cnt >= m:
+                answer += 1
+                break
+
+print(answer)
