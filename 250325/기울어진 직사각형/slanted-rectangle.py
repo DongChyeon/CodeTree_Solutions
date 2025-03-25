1e0e6b90c9018843bc75b0a815de73c2a9f5d6a2
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-03-25 20:37:15 +0900

    Title: 기울어진 직사각형, Time: 152ms, Memory: 21MB, Status: Passed - CodeTree

--- 250325/기울어진 직사각형/slanted-rectangle.py
+++ 250325/기울어진 직사각형/slanted-rectangle.py
@@ -3,54 +3,39 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 
 answer = 0
 
-dx = [1, -1, -1, 1]
-dy = [-1, -1, 1, 1]
-
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
 
-def traverse(x, y, k):
-    global answer
+def traverse(x, y, w, h):
+    dx = [1, -1, -1, 1]
+    dy = [-1, -1, 1, 1]
 
-    for length1 in range(1, k):
-        visited = [[0] * n for _ in range(n)]
-        visited[y][x] = 1
+    total = 0
+    for i in range(4):
+        if i % 2 == 0:
+            for _ in range(w):
+                nx, ny = x + dx[i], y + dy[i]
+                if in_range(nx, ny):
+                    x, y = nx, ny
+                    total += grid[y][x]
+                else:
+                    return 0
+        else:
+            for _ in range(h):
+                nx, ny = x + dx[i], y + dy[i]
+                if in_range(nx, ny):
+                    x, y = nx, ny
+                    total += grid[y][x]
+                else:
+                    return 0
+    
+    return total
 
-        length2 = k - length1
-
-        can_establish = True
-        for d in range(4):
-            if d % 2 == 0:
-                for _ in range(length1):
-                    nx, ny = x + dx[d], y + dy[d]
-                    if in_range(nx, ny):
-                        x, y = nx, ny
-                        visited[ny][nx] = 1
-                    else:
-                        can_establish = False
-            else:
-                for _ in range(length2):
-                    nx, ny = x + dx[d], y + dy[d]
-                    if in_range(nx, ny):
-                        x, y = nx, ny
-                        visited[ny][nx] = 1
-                    else:
-                        can_establish = False
-
-        if can_establish:
-            sum_of_value = sum([
-                grid[y][x]
-                for y in range(n)
-                for x in range(n)
-                if visited[y][x] == 1
-            ])
-
-            if sum_of_value > answer:
-                answer = sum_of_value
                 
 for y in range(n):
     for x in range(n):
-        for k in range(2, n + 2):
-            traverse(x, y, k)
+        for w in range(1, n):
+            for h in range(1, n):
+                answer = max(answer, traverse(x, y, w, h))
 
 print(answer)
