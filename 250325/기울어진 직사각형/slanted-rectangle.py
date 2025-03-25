c8afde175b5df81ede9956c36f293dba0dc20b2e
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-03-25 14:41:13 +0900

    Title: 기울어진 직사각형, Time: 56ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250325/기울어진 직사각형/slanted-rectangle.py
@@ -0,0 +1,59 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+answer = 0
+
+dx = [1, -1, -1, 1]
+dy = [-1, -1, 1, 1]
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+def traverse(x, y, k):
+    global answer
+
+    for length1 in range(1, k):
+        visited = [[0] * n for _ in range(n)]
+
+        length2 = k - length1
+
+        can_establish = True
+        for d in range(4):
+            if d % 2 == 0:
+                for i in range(length1):
+                    nx, ny = x + dx[d], y + dy[d]
+                    if in_range(nx, ny):
+                        x, y = nx, ny
+                        visited[ny][nx] = 1
+                    else:
+                        can_establish = False
+                        break
+            else:
+                for i in range(length2):
+                    nx, ny = x + dx[d], y + dy[d]
+                    if in_range(nx, ny):
+                        x, y = nx, ny
+                        visited[ny][nx] = 1
+                    else:
+                        can_establish = False
+                        break
+
+            if not can_establish:
+                break
+
+        if can_establish:
+            sum_of_value = sum([
+                grid[y][x]
+                for y in range(n)
+                for x in range(n)
+                if visited[y][x] == 1
+            ])
+
+            answer = max(answer, sum_of_value)
+
+for y in range(n):
+    for x in range(n):
+        for k in range(2, n + 1):
+            traverse(x, y, k)
+
+print(answer)
