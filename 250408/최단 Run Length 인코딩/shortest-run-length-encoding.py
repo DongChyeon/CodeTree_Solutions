c652c70b89a0dc7eb1d1501ebef9a554f6abf63a
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-04-08 09:40:10 +0900

    Title: 최단 Run Length 인코딩, Time: 56ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250408/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -0,0 +1,29 @@
+from collections import deque
+import sys
+
+A = deque(list(input()))
+answer = sys.maxsize
+
+temp = deque(list(A))
+for i in range(1, len(A)):
+    temp.rotate(1)
+
+    encoding = []
+    last_ch = temp[0]
+    count = 1
+    for j in range(1, len(temp)):
+        if temp[j] == last_ch:
+            count += 1
+        else:
+            encoding.append(last_ch)
+            encoding.append(count)
+
+            last_ch = temp[j]
+            count = 1
+
+    encoding.append(last_ch)
+    encoding.append(count)
+
+    answer = min(answer, len(''.join(map(str, encoding))))
+
+print(answer)
