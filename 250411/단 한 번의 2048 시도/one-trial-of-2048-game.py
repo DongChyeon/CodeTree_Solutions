dcb07b79c05c25378250f31ff819747e1abfeaea
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-04-11 15:59:35 +0900

    Title: 단 한 번의 2048 시도, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250411/단 한 번의 2048 시도/one-trial-of-2048-game.py
+++ 250411/단 한 번의 2048 시도/one-trial-of-2048-game.py
@@ -1,86 +1,73 @@
-# Read 4x4 grid
-grid = [list(map(int, input().split())) for _ in range(4)]
+NONE = -1
 
-# Read direction
+n = 4
+grid = [list(map(int, input().split())) for _ in range(n)]
+next_grid = [[0 for _ in range(n)] for _ in range(n)]
+
+def rotate():
+    for y in range(n):
+        for x in range(n):
+            next_grid[y][x] = 0
+
+    for y in range(n):
+        for x in range(n):
+            next_grid[y][x] = grid[n - x - 1][y]
+
+    for y in range(n):
+        for x in range(n):
+            grid[y][x] = next_grid[y][x]
+
+def drop():
+    for y in range(n):
+        for x in range(n):
+            next_grid[y][x] = 0
+
+    for x in range(n):
+        keep_num, next_row = NONE, n - 1
+
+        for y in range(n - 1, -1, -1):
+            if grid[y][x] == 0:
+                continue
+
+            if keep_num == NONE:
+                keep_num = grid[y][x]
+            elif keep_num == grid[y][x]:
+                next_grid[next_row][x] = keep_num * 2
+                keep_num = NONE
+
+                next_row -= 1
+            else:
+                next_grid[next_row][x] = keep_num
+                keep_num = grid[y][x]
+
+                next_row -= 1
+
+        if keep_num != NONE:
+            next_grid[next_row][x] = keep_num
+            next_row -= 1
+
+    for y in range(n):
+        for x in range(n):
+            grid[y][x] = next_grid[y][x]
+
+def tilt(move_direction):
+    direction_mapper = {
+        'D': 0,
+        'R': 1,
+        'U': 2,
+        'L': 3
+    }
+
+    for _ in range(direction_mapper[move_direction]):
+        rotate()
+
+    drop()
+
+    for _ in range(4 - direction_mapper[move_direction]):
+        rotate()
+
 direction = input()
-
-# Please write your code here.
-if direction == 'L' or direction == 'R':
-    for row in range(4):
-        numbers = [num for num in grid[row] if num != 0]
-        temp = []
-
-        if direction == 'L':
-            i = 0
-            while i < len(numbers):
-                if numbers[i] == 0:
-                    i += 1
-                elif i < len(numbers) -1 and numbers[i] == numbers[i + 1]:
-                    temp.append(numbers[i] * 2)
-                    i += 2
-                else:
-                    temp.append(numbers[i])
-                    i += 1
-        else:
-            i = len(numbers) - 1
-            while i > -1:
-                if numbers[i] == 0:
-                    i -= 1
-                elif i > 0 and numbers[i] == numbers[i - 1]:
-                    temp.append(numbers[i] * 2)
-                    i -= 2
-                else:
-                    temp.append(numbers[i])
-                    i -= 1
-    
-        if direction == 'L':
-            while len(temp) < 4:
-                temp.append(0)
-            grid[row] = temp
-        else:
-            while len(temp) < 4:
-                temp.append(0)
-            grid[row] = temp[::-1]
-
-else:
-    for col in range(4):
-        numbers = [grid[row][col] for row in range(4) if grid[row][col] != 0]
-        temp = []
-
-        if direction == 'U':
-            i = 0
-            while i < len(numbers):
-                if numbers[i] == 0:
-                    i += 1
-                elif i < len(numbers) -1 and numbers[i] == numbers[i + 1]:
-                    temp.append(numbers[i] * 2)
-                    i += 2
-                else:
-                    temp.append(numbers[i])
-                    i += 1
-        else:
-            i = len(numbers) - 1
-            while i > -1:
-                if numbers[i] == 0:
-                    i -= 1
-                elif i > 0 and numbers[i] == numbers[i - 1]:
-                    temp.append(numbers[i] * 2)
-                    i -= 2
-                else:
-                    temp.append(numbers[i])
-                    i -= 1
-    
-        if direction == 'U':
-            while len(temp) < 4:
-                temp.append(0)
-            for i in range(4):
-                grid[i][col] = temp[i]
-        else:
-            while len(temp) < 4:
-                temp.append(0)
-            temp = temp[::-1]
-            for i in range(4):
-                grid[i][col] = temp[i]
+tilt(direction)
 
 for row in grid:
     for col in row:
