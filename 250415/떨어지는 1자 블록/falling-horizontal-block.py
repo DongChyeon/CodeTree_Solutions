7853c883ca39064279ccdaf0f227364e096045f4
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-04-15 12:03:08 +0900

    Title: 떨어지는 1자 블록, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250415/떨어지는 1자 블록/falling-horizontal-block.py
+++ 250415/떨어지는 1자 블록/falling-horizontal-block.py
@@ -1,28 +1,22 @@
 n, m, k = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-block_row = 0
-for x in range(k - 1, k - 1 + m):
-    grid[block_row][x] = 1
+def is_all_blank(row, start_col, end_col):
+    return all([
+        not grid[row][col]
+        for col in range(start_col, end_col + 1)
+    ])
 
-def can_go_down(k, m, row):
-    if row == n - 1:
-        return False
+def get_target_row():
+    for row in range(n):
+        if not is_all_blank(row, k - 1, k + m - 2):
+            return row - 1
 
-    for x in range(k - 1, k - 1 + m):
-        if grid[row + 1][x] == 1:
-            return False
+    return n - 1
 
-    return True
-
-while True:
-    if can_go_down(k, m, block_row):
-        block_row += 1
-        for x in range(k - 1, k - 1 + m):
-            grid[block_row - 1][x] = 0
-            grid[block_row][x] = 1
-    else:
-        break
+block_row = get_target_row()
+for col in range(k - 1, k - 1 + m):
+    grid[block_row][col] = 1
 
 for row in grid:
     for col in row:
