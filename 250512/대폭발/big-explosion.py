c49b24aa104dce17ab9d60f789995e4a312c6c80
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-05-12 13:22:09 +0900

    Title: 대폭발, Time: 54ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250512/대폭발/big-explosion.py
@@ -0,0 +1,34 @@
+n, m, r, c = map(int, input().split())
+r, c = r - 1, c - 1
+grid = [[0 for _ in range(n)] for _ in range(n)]
+grid[r][c] = 1
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+for sec in range(1, m + 1):
+    bomb_pos = [
+        (x, y)
+        for y in range(n)
+        for x in range(n)
+        if grid[y][x] == 1
+    ]
+
+    for pos in bomb_pos:
+        x, y = pos
+        if grid[y][x] == 1:
+            for i in range(4):
+                nx, ny = x + (sec * dx[i]), y + (sec * dy[i])
+                if in_range(nx, ny) and grid[ny][nx] != 1:
+                    grid[ny][nx] = 1
+
+answer = 0
+for y in range(n):
+    for x in range(n):
+        if grid[y][x] == 1:
+            answer += 1
+
+print(answer)
