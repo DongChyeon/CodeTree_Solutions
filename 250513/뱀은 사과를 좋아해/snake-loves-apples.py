2641ffcdeff4e7cb959ea3a297633b3cb2631698
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-05-13 15:52:39 +0900

    Title: 뱀은 사과를 좋아해, Time: 91ms, Memory: 18MB, Status: Passed - Codetree

--- 250513/뱀은 사과를 좋아해/snake-loves-apples.py
+++ 250513/뱀은 사과를 좋아해/snake-loves-apples.py
@@ -15,6 +15,13 @@ def solution():
         r, c = map(int, input().split())
         grid[r - 1][c - 1] = 2
 
+    def print_grid():
+        for row in grid:
+            for col in row:
+                print(col, end=' ')
+            print()
+        print()
+
     def in_range(x, y):
         return 0 <= x < n and 0 <= y < n
 
@@ -33,12 +40,11 @@ def solution():
         distance = int(distance)
         
         for _ in range(distance):
+            #print_grid()
             answer += 1
             nx, ny = x + dx[direction_mapper[direction]], y + dy[direction_mapper[direction]]
 
             if not in_range(nx, ny):
-                return answer
-            elif grid[ny][nx] == 1:
                 return answer
             elif grid[ny][nx] == 2:
                 grid[ny][nx] = 1
@@ -52,6 +58,9 @@ def solution():
                     tail_x, tail_y = tail_pos.popleft()
                     grid[tail_y][tail_x] = 0
                     tail_pos.append((x, y))
+
+                if grid[ny][nx] == 1:
+                    return answer
 
                 grid[ny][nx] = 1
                 x, y = nx, ny
