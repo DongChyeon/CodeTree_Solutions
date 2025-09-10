1ac010cb2032b7161d61898511b4bb440be795c9
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-09-10 17:40:04 +0900

    Title: 그래프 탐색, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250910/그래프 탐색/graph-traversal.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+graph = [[] for _ in range(n)]
+for _ in range(m):
+    v1, v2 = map(int, input().split())
+    graph[v1 - 1].append(v2 - 1)
+    graph[v2 - 1].append(v1 - 1)
+
+visited = [False] * n
+
+answer = 0
+
+def dfs(vertex):
+    global answer
+
+    for curr_v in graph[vertex]:
+        if not visited[curr_v]:
+            answer += 1
+            visited[curr_v] = True
+            dfs(curr_v)
+
+dfs(0)
+# 시작점 제외
+print(answer - 1)
