b98ed9d4efc2ca592d9e45f5ea25ec479c2872ab
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-10-15 19:42:47 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

--- 251015/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
+++ 251015/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
@@ -2,24 +2,23 @@ n = int(input())
 curr = '4'
 
 def is_possible(curr):
-    for length in range(1, len(curr) // 2 + 1):
-        for start in range(len(curr) - 1):
-            if curr[start:start + length] == curr[start + length:start + 2 * length]:
-                return False
+    half = len(curr) // 2
+
+    for i in range(1, half + 1):
+        if curr[-i:] == curr[-2 * i:-i]:
+            return False
 
     return True
 
 def dfs(curr, depth, last_char):
     if depth == n :
-        if is_possible(curr):
-            print(curr)
-            exit(0)
-        return
+        print(curr)
+        exit(0)
 
     for num in ['4', '5', '6']:
-        if num != last_char:
-            curr += num
+        curr += num
+        if is_possible(curr):
             dfs(curr, depth + 1, num)
-            curr = curr[:-1]
+        curr = curr[:-1]
 
 dfs(curr, 1, '4')
