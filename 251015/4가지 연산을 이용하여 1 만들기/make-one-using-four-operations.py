1226d0566f5ab821c532057880b5fcf46ba7bd82
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-10-15 20:29:39 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 99ms, Memory: 30MB, Status: Passed - Codetree

--- 251015/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
+++ 251015/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -1,11 +1,9 @@
 from collections import deque
 
 N = int(input())
-visited = [False] * (N + 1)
+MAX = 2 * N + 2
+visited = [False] * MAX
 visited[N] = True
-
-def in_range(num):
-    return 0 <= num <= N
 
 def bfs():
     queue = deque([(N, 0)])
@@ -15,18 +13,27 @@ def bfs():
         if num == 1:
             return cnt
 
-        if num % 3 == 0 and not visited[num // 3]:
-            visited[num // 3] = True
-            queue.append((num // 3, cnt + 1))
-        if num % 2 == 0 and not visited[num // 2]:
-            visited[num // 2] = True
-            queue.append((num // 2, cnt + 1))
-        if in_range(num - 1) and not visited[num - 1]:
-            visited[num - 1] = True
-            queue.append((num - 1, cnt + 1))
-        if in_range(num + 1) and not visited[num + 1]:
-            visited[num + 1] = True
-            queue.append((num + 1, cnt + 1))
+        if num % 3 == 0:
+            nxt = num // 3
+            if not visited[nxt]:
+                visited[nxt] = True
+                queue.append((nxt, cnt + 1))
+
+        if num % 2 == 0:
+            nxt = num // 2
+            if not visited[nxt]:
+                visited[nxt] = True
+                queue.append((nxt, cnt + 1))
+
+        nxt = num - 1
+        if nxt >= 0 and not visited[nxt]:
+            visited[nxt] = True
+            queue.append((nxt, cnt + 1))
+
+        nxt = num + 1
+        if nxt < MAX and not visited[nxt]:
+            visited[nxt] = True
+            queue.append((nxt, cnt + 1))
         
     return -1
 
