24da6d837f2d55e05962a7ba195119e820e01d60
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-10-20 23:23:30 +0900

    Title: 삽입 정렬 구현, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 251020/삽입 정렬 구현/implement-insertion-sort.py
+++ 251020/삽입 정렬 구현/implement-insertion-sort.py
@@ -6,7 +6,7 @@ for i in range(1, len(arr)):
     j = i - 1
     key = arr[i]
     while j >= 0 and arr[j] > key:
-        arr[j + 1], arr[j] = arr[j], arr[j + 1]
+        arr[j + 1] = arr[j]
         j -= 1
     arr[j + 1] = key
 
