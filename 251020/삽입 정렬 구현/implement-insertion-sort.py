560d1d47eeb995cd6f529042772523b1bb71ac54
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-10-20 23:22:38 +0900

    Title: 삽입 정렬 구현, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251020/삽입 정렬 구현/implement-insertion-sort.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = list(map(int, input().split()))
+#print(arr)
+
+for i in range(1, len(arr)):
+    j = i - 1
+    key = arr[i]
+    while j >= 0 and arr[j] > key:
+        arr[j + 1], arr[j] = arr[j], arr[j + 1]
+        j -= 1
+    arr[j + 1] = key
+
+print(' '.join(map(str, arr)))
