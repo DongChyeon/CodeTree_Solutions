28dc890173179e8d218bc31d17a40f425a96406a
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-10-24 13:30:52 +0900

    Title: 벽이 없는 충돌 실험, Time: 182ms, Memory: 140MB, Status: Wrong Answer - Codetree

--- 251024/벽이 없는 충돌 실험/collision-experiment-without-wall.py
+++ 251024/벽이 없는 충돌 실험/collision-experiment-without-wall.py
@@ -1,42 +1,85 @@
-from collections import defaultdict
+COORD_SIZE = 4000
+OFFSET = 2000
+BLANK = -1
 
 T = int(input())
 dir_mapper = { 'U': 0, 'D': 1, 'R': 2, 'L': 3 }
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
+next_marbles_index = [[BLANK for _ in range(COORD_SIZE)] for _ in range(COORD_SIZE)]
+
+marbles = []
+next_marbles = []
+
+last_collision_time = -1
+
+def move(marble):
+    dx = [0, 0, 1, -1]
+    dy = [1, -1, 0, 0]
+
+    x, y, w, dir_idx, num = marble
+    nx, ny = x + dx[dir_idx], y + dy[dir_idx]
+
+    return (nx, ny, w, dir_idx, num)
+
+def find_duplicate_marble(marble):
+    target_x, target_y, _, _, _ = marble
+
+    return next_marbles_index[target_y][target_x]
+
+def collide(marble1, marble2):
+    _, _, weight1, _, num1 = marble1
+    _, _, weight2, _, num2 = marble2
+
+    if weight1 > weight2 or (weight1 == weight2 and num1 > num2):
+        return marble1
+    else:
+        return marble2
+
+def is_out_of_active_coordinate(marble):
+    x, y, _, _, _ = marble
+    return x < 0 or x > COORD_SIZE - 1 or y < 0 or y > COORD_SIZE - 1
+
+def push_next_marble(marble, curr_time):
+    global last_collision_time
+
+    if is_out_of_active_coordinate(marble):
+        return
+
+    index = find_duplicate_marble(marble)
+
+    if index == BLANK:
+        next_marbles.append(marble)
+
+        x, y, _, _, _ = marble
+        next_marbles_index[y][x] = len(next_marbles) - 1
+    else:
+        next_marbles[index] = collide(next_marbles[index], marble)
+        last_collision_time = curr_time
 
 for _ in range(T):
     N = int(input())
-    positions = {}
+    last_collision_time = -1
 
+    # 위치 초기화
     for i in range(N):
         xi, yi, wi, di = input().split()
         
         dir_idx = dir_mapper[di]
-        x = int(xi) * 2
-        y = int(yi) * 2
+        x = int(xi) * 2 + OFFSET
+        y = int(yi) * 2 + OFFSET
         w = int(wi)
 
-        positions[(x, y)] = (w, i, dir_idx) # 무게, 순서, 방향
+        marbles.append((x, y, w, dir_idx, i + 1))
 
-    answer = 0
-    for t in range(1, 4001):
-        new_positions = defaultdict(list)
-        for (x, y), (w, seq, dir_idx) in positions.items():
-            nx, ny = x + dx[dir_idx], y + dy[dir_idx]
-            new_positions[(nx, ny)].append((w, seq, dir_idx))
+    for t in range(1, COORD_SIZE + 1):
+        for marble in marbles:
+            next_marble = move(marble)
+            push_next_marble(next_marble, t)
 
-        positions.clear()
-        for (nx, ny), lst in new_positions.items():
-            if len(lst) > 1:
-                answer = t
-                w, seq, dir_idx = max(lst, key = lambda b: (b[0], b[1]))
-                positions[(nx, ny)] = (w, seq, dir_idx)
-            else:
-                positions[(nx, ny)] = lst[0]
+        marbles = next_marbles[:]
 
-    if answer > 0:
-        print(answer)
+        for x, y, _, _, _ in next_marbles:
+            next_marbles_index[y][x] = BLANK
 
-    if answer == 0:
-        print(-1)
+        next_marbles = []
+
+    print(last_collision_time)
