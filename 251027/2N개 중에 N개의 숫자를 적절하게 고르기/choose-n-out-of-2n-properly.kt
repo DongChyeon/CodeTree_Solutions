490a58f854089fe4ecc2f4cee9c981ad2621beda
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-10-27 20:00:06 +0900

    Title: 2N개 중에 N개의 숫자를 적절하게 고르기, Time: 143ms, Memory: 12MB, Status: Wrong Answer - Codetree

---
+++ 251027/2N개 중에 N개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.kt
@@ -0,0 +1,28 @@
+import kotlin.math.abs
+
+fun main() {
+    val n = readLine()!!.toInt()
+    val nums = readLine()!!.split(" ").map { it.toInt() }
+    val sumOfNums = nums.sum()
+
+    var answer = sumOfNums
+
+    fun dfs(start: Int, num: Int, depth: Int) {
+        if (answer == 0) return
+
+        if (depth == n) {
+            val diff = abs((sumOfNums - num) - num)
+            if (diff < answer) answer = diff
+            return
+        }
+
+        for (i in start until nums.size) {
+            val targetNum = nums[i]
+            dfs(start, num + targetNum, depth + 1)
+        }
+    }
+
+    dfs(0, 0, 0)
+
+    println(answer)
+}
