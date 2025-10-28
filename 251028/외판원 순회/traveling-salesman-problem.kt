e0a369265fc5211361cb692544c3681f796579ce
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-10-28 12:00:00 +0900

    Title: 외판원 순회, Time: 160ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 251028/외판원 순회/traveling-salesman-problem.kt
@@ -0,0 +1,51 @@
+import kotlin.math.min
+
+fun main() {
+    val n = readLine()!!.trim().toInt()
+    val cost = Array(n) { 
+        readln().trim().split(" ").map { it.toInt() }
+    }
+
+    // 0-based Index (출발점을 0으로)
+    val choosedPlaces: MutableList<Int> = mutableListOf(0)
+    val visited = BooleanArray(n) { false }
+
+    var answer = Int.MAX_VALUE
+
+    fun getCost(): Int {
+        var result = 0
+
+        for (i in 0 until choosedPlaces.size - 1) {
+            val prevPlace = choosedPlaces[i]
+            val curPlace = choosedPlaces[i + 1]
+
+            result += cost[prevPlace][curPlace]
+        }
+
+        result += cost[choosedPlaces.last()][0]
+
+        return result
+    }
+
+    fun choose(depth: Int) {
+        if (depth == n) {
+            answer = min(answer, getCost())
+            return
+        }
+
+        for (i in 1 until n) {
+            if (visited[i]) continue
+
+            choosedPlaces.add(i)
+            visited[i] = true
+            choose(depth + 1)
+            choosedPlaces.removeLast()
+            visited[i] = false
+        }
+
+    }
+
+    choose(1)
+
+    println(answer)
+}
