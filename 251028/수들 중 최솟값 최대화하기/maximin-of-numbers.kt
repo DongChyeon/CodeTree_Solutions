40dc038afe48424590cdd82d1d58c5bc236f3292
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-10-28 13:28:42 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 131ms, Memory: 12MB, Status: Runtime Error - Codetree

---
+++ 251028/수들 중 최솟값 최대화하기/maximin-of-numbers.kt
@@ -0,0 +1,29 @@
+import kotlin.math.max
+import kotlin.math.min
+
+fun main() {
+    val n = readLine()!!.toInt()
+    val grid = List(n) { readln().split(" ").map { it.toInt() } }
+
+    var answer = 0
+    val visited = BooleanArray(n) { false }
+
+    fun choose(depth: Int, minNum: Int) {
+        if (depth == n) {
+            answer = max(answer, minNum)
+            return
+        }
+
+        for (num in 0 until n) {
+            if (visited[num]) continue
+
+            visited[num] = true
+            choose(depth + 1, min(minNum, grid[depth][num]))
+            visited[num] = false
+        }
+    }
+
+    choose(0, Int.MAX_VALUE)
+
+    println(answer)
+}
