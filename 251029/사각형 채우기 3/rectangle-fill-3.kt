cc5f3b3c33df1382aff8a864a94fa613e0768ae6
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-10-29 14:54:52 +0900

    Title: 사각형 채우기 3, Time: 101ms, Memory: 9MB, Status: Passed - Codetree

--- 251029/사각형 채우기 3/rectangle-fill-3.kt
+++ 251029/사각형 채우기 3/rectangle-fill-3.kt
@@ -1,14 +1,16 @@
 fun main() {
     val n = readLine()!!.toInt()
-    val dp = IntArray(n + 1) { 0 }
+    val dp = LongArray(n + 1) { 0 }
+
+    val DIVIDER = 1000000007
 
     dp[0] = 1
     dp[1] = 2
 
     for (i in 2 until n + 1) {
-        dp[i] = (dp[i - 1] * 2 + dp[i - 2] * 3) % 1000000007
+        dp[i] = (dp[i - 1] * 2 + dp[i - 2] * 3) % DIVIDER
         for (j in i - 3 downTo 0) {
-            dp[i] = (dp[i] + dp[j] * 2) % 1000000007
+            dp[i] = (dp[i] + dp[j] * 2) % DIVIDER
         }
     }
 
