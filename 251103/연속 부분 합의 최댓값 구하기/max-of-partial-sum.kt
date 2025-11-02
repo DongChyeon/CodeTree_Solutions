e65b2b4b6413966df4687261bb6e2e96b6318ded
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-11-03 00:15:58 +0900

    Title: 연속 부분 합의 최댓값 구하기, Time: 409ms, Memory: 27MB, Status: Passed - Codetree

--- 251103/연속 부분 합의 최댓값 구하기/max-of-partial-sum.kt
+++ 251103/연속 부분 합의 최댓값 구하기/max-of-partial-sum.kt
@@ -4,12 +4,15 @@ fun main() {
     val n = readln().trim().toInt()
     val a = readln().trim().split(" ").map { it.toInt() }
 
+    var answer = a[0]
+
     val dp = IntArray(n) { 0 }
     dp[0] = a[0]
 
     for (i in 1 until n) {
         dp[i] = max(dp[i - 1] + a[i], a[i])
+        answer = max(answer, dp[i])
     }
 
-    println(dp[n - 1])
+    println(answer)
 }
