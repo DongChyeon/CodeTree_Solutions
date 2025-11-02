d308a04fdc8fb0698b601256a6325971b3cd1ff1
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-11-03 01:11:02 +0900

    Title: 계단 오르기 2, Time: 188ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 251103/계단 오르기 2/climbing-stairs-2.kt
+++ 251103/계단 오르기 2/climbing-stairs-2.kt
@@ -7,8 +7,8 @@ fun main() {
     val dp = List(n + 1) { IntArray(4) { 0 }}
 
     for (i in 1 until n + 1) {
-        for (j in 1 until 4) {
-            dp[i][j] = dp[i - 1][j - 1] + coins[i - 1]
+        for (j in 0 until 4) {
+            if (j > 0 && i >= j) dp[i][j] = dp[i - 1][j - 1] + coins[i - 1]
             if (i > 1) dp[i][j] = max(dp[i][j], dp[i - 2][j] + coins[i - 2])
         }
     }
