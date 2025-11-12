d5fea2037bce267f7ba8a66d0ba9de986c4dbe5e
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-11-12 16:50:04 +0900

    Title: 최대 증가 부분 수열, Time: 155ms, Memory: 13MB, Status: Runtime Error - Codetree

---
+++ 251112/최대 증가 부분 수열/longest-increasing-subsequence.kt
@@ -0,0 +1,22 @@
+import kotlin.math.max
+
+fun main() {
+    val n = readLine()!!.toInt()
+    val sequence = readln().split(" ").map { it.toInt() }
+    val dp = IntArray(n) { 0 }
+    dp[0] = 1
+
+    for (i in 1 until n) {
+        var maxLength = 0
+
+        for (j in 0 until i) {
+            if (sequence[i] > sequence[j]) {
+                dp[i] = max(dp[i], dp[j] + 1)
+            } else {
+                dp[i] = max(dp[i], dp[j])
+            }
+        }
+    }
+
+    println(dp[n - 1])
+}
