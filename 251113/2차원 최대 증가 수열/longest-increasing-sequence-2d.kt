2cf8a98ce7b0ffb8d43c7d35f9e6b8f92217f110
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-11-13 15:35:40 +0900

    Title: 2차원 최대 증가 수열, Time: 248ms, Memory: 19MB, Status: Wrong Answer - Codetree

---
+++ 251113/2차원 최대 증가 수열/longest-increasing-sequence-2d.kt
@@ -0,0 +1,20 @@
+import kotlin.math.max
+
+fun main() {
+    val (n, m) = readln().trim().split(" ").map { it.toInt() }
+    val grid = Array(n) { readln().trim().split(" ").map { it.toInt() }.toIntArray() }
+    val dp = Array(n) { IntArray(m) { 1 } }
+
+    for (i in 1 until n) {
+        for (j in 1 until m) {
+            for (y in 0 until i) {
+                for (x in 0 until j) {
+                    if (grid[i][j] > grid[y][x]) dp[i][j] = max(dp[i][j], dp[y][x] + 1)
+                }
+            }
+        }
+    }
+    
+    //for (row in dp) println(row.joinToString(" "))
+    println(dp.map { it.toList() }.flatten().maxOrNull()!!)
+}
