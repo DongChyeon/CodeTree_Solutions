ab6085aa57d4b17ce6a973eafcb677b7eed074c6
Donghyeon Kim <kimdonghyun90766@gmail.com>
2025-11-18 14:00:07 +0900

    Title: 증가했다가 감소하는 부분 수열, Time: 236ms, Memory: 17MB, Status: Passed - Codetree

--- 251118/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.kt
+++ 251118/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.kt
@@ -12,37 +12,14 @@ fun main() {
             if (arr[i] > arr[j]) {
                 dp1[i] = max(dp1[i], dp1[j] + 1)
             }
-            else if (arr[i] < arr[j]) {
-                dp2[i] = max(dp2[i], dp2[j] + 1)
-            }
-        }
-    }
 
-    val maxAscendingSequenceValue = dp1.maxOrNull()!!
-    val maxDescendingSequenceValue = dp2.maxOrNull()!!
-
-    val ascendingEndIndex = dp1.indexOf(maxAscendingSequenceValue)
-
-    if (ascendingEndIndex == n - 1) {
-        println(max(maxAscendingSequenceValue, maxDescendingSequenceValue))
-        return
-    }
-
-    for (i in 0 until n) dp2[i] = 0
-    dp2[ascendingEndIndex + 1] = 1
-
-    for (i in ascendingEndIndex + 2 until n) {
-        for (j in 0 until i) {
             if (arr[i] < arr[j]) {
                 dp2[i] = max(dp2[i], dp2[j] + 1)
             }
         }
+        
+        dp2[i] = max(dp1[i], dp2[i])
     }
 
-    var answer = max(
-        maxAscendingSequenceValue + dp2[n - 1],
-        maxDescendingSequenceValue
-    )
-
-    println(answer)
+    println(dp2.maxOrNull()!!)
 }
